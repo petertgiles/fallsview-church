79e5e8b58439a58293b5afb22a8e34b0f4e7a0a6
Peter Giles <petertgiles@gmail>
2024-05-12 16:38:20 -0400

    tutorial part 3

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+module.exports = function(eleventyConfig) {
+    eleventyConfig.addPassthroughCopy("src/assets/css/style.css");
+    eleventyConfig.addPassthroughCopy("src/assets/images");
+
+  
+    return {
+      dir: {
+        input: "src",
+        data: "_data",
+        includes: "_includes",
+        layouts: "_layouts",
+      },
+    };
+  };
+  
+  
